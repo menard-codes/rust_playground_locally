e1913800fd6657cae335c3882123b96a7069960e
Menard Maranan <menardmaranan.codes@gmail.com>
2022-07-17 01:39:43 +0800

    uses playground api

--- run_rust.py
+++ run_rust.py
@@ -3,17 +3,20 @@ import requests
 import json
 
 def run_rust_code(code: str) -> None:
-    URL = "https://play.rust-lang.org/evaluate.json"
+    URL = "https://play.rust-lang.org/execute"
     payload = {
-        "version": "stable",
-        "optimize": "0",
+        "channel": "stable",
+        "mode": "debug",
+        "edition": "2021",
+        "crateType": "bin",
+        "tests": False,
         "code": code,
-        "edition": "2015"
+        "backtrace": False
     }
     data = json.dumps(payload)
     headers = {"Content-type": "application/json"}
     response = requests.post(url=URL, data=data, headers=headers)
-    print(response.json()["result"])
+    print(response.json()["stdout"])
 
 def main():
     code_input = argv[1]
